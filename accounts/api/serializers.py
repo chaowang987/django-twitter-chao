b3674ebf31e975551845fdca3e69de9c8cb08559
chaowang987 <chaowang987@gmail.com>
2022-05-19 18:01:17 -0400

    Login, login_status, logout, signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,6 +1,55 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+from django.contrib.auth.models import User
+from rest_framework import serializers, exceptions
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'password']
+        fields = ['username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'User does not exist.'
+            })
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    # will be called when is_valid is called
+    # this is to make sure username and email not case-sensitive
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        # store the user/email with lower so query can take less time
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
--- twitter/settings.py
+++ twitter/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATOIN_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
