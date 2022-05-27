09341781061852a49e421adf785cf5ae5cbd5c0a
chaowang987 <chaowang987@gmail.com>
2022-05-27 01:44:51 -0400

    add followings, followers api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -12,6 +12,8 @@ class UserSerializerForTweet(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username')
 
+class UserSerializerForFriendship(UserSerializerForTweet):
+    pass
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
