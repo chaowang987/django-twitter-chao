904c6e4ff5a316490f2402bbfd2a419a3c99c9e6
chaowang987 <chaowang987@gmail.com>
2022-05-30 20:59:24 -0400

    add retrieve, decorator and tests

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,14 +1,10 @@
+from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
 from rest_framework import serializers
 from tweets.models import Tweet
-from accounts.api.serializers import UserSerializerForTweet
 
 
 class TweetSerializer(serializers.ModelSerializer):
-    # "user": {
-    #     "id": 6,
-    #     "username": "twitter_test1"
-    # }
-    # 如果不写就是只有 "user": 6, 不会再展开
     user = UserSerializerForTweet()
 
     class Meta:
@@ -30,3 +26,10 @@ class TweetSerializerForCreate(serializers.ModelSerializer):
         content = validated_data['content']
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
+
+class TweetSerializerWithComments(serializers.ModelSerializer):
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'comments', 'created_at', 'content',)
