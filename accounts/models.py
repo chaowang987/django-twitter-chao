f73257ba1a59cf5c04937cf2684e73239f832cbb
chaowang987 <chaowang987@gmail.com>
2022-06-29 01:51:46 -0400

    add userprofile admin, models and tests

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class UserProfile:
+class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
     avatar = models.FileField(null=True)
