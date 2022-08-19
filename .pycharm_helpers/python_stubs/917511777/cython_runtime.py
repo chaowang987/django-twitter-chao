4448fe92e420e0552250be6090a7a63f94ba1a73
chaowang987 <chaowang987@gmail.com>
2022-08-19 00:28:53 -0400

    add HBaseModel and friendship HBaseModel

--- .pycharm_helpers/python_stubs/917511777/cython_runtime.py
+++ .pycharm_helpers/python_stubs/917511777/cython_runtime.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 # module cython_runtime
-# from /usr/lib/python3/dist-packages/twisted/test/raiser.cpython-36m-x86_64-linux-gnu.so
+# from /home/vagrant/.local/lib/python3.6/site-packages/thriftpy2/transport/memory/cymemory.cpython-36m-x86_64-linux-gnu.so
 # by generator 1.147
 # no doc
 # no imports
