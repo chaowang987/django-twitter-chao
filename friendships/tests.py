62b20f40042461bf14232ebf144423a4e4c74ea8
chaowang987 <chaowang987@gmail.com>
2022-08-20 00:43:05 -0400

    add hbase filter and unit tests

--- friendships/tests.py
+++ friendships/tests.py
@@ -88,3 +88,49 @@ class HBaseTests(TestCase):
             exception_raised = True
             self.assertEqual(str(e), 'created_at is missing in the row key.')
         self.assertEqual(exception_raised, True)
+
+    def test_filter(self):
+        for i in range(2, 5):
+            HBaseFollowing.create(from_user_id=1, to_user_id=i, created_at=self.ts_now)
+
+        followings = HBaseFollowing.filter(prefix=(1, None))
+        self.assertEqual(len(followings), 3)
+        self.assertEqual(followings[0].from_user_id, 1)
+        self.assertEqual(followings[0].to_user_id, 2)
+        self.assertEqual(followings[1].from_user_id, 1)
+        self.assertEqual(followings[1].to_user_id, 3)
+        self.assertEqual(followings[2].from_user_id, 1)
+        self.assertEqual(followings[2].to_user_id, 4)
+
+        # test limit
+        filtered = HBaseFollowing.filter(prefix=(1, None), limit=1)
+        self.assertEqual(len(filtered), 1)
+        self.assertEqual(filtered[0].from_user_id, 1)
+        self.assertEqual(filtered[0].to_user_id, 2)
+
+        filtered = HBaseFollowing.filter(prefix=(1, None), limit=2)
+        self.assertEqual(len(filtered), 2)
+        self.assertEqual(filtered[0].to_user_id, 2)
+        self.assertEqual(filtered[1].to_user_id, 3)
+
+        filtered = HBaseFollowing.filter(prefix=(1, None, None), limit=4)
+        self.assertEqual(len(filtered), 3)
+        self.assertEqual(filtered[0].to_user_id, 2)
+        self.assertEqual(filtered[1].to_user_id, 3)
+        self.assertEqual(filtered[2].to_user_id, 4)
+        
+        filtered = HBaseFollowing.filter(start=(1, filtered[1].created_at), limit=2)
+        self.assertEqual(len(filtered), 2)
+        self.assertEqual(filtered[0].to_user_id, 3)
+        self.assertEqual(filtered[1].to_user_id, 4)
+
+        # test reverse
+        reversed = HBaseFollowing.filter(prefix=(1, None), limit=2, reverse=True)
+        self.assertEqual(len(reversed), 2)
+        self.assertEqual(reversed[0].to_user_id, 4)
+        self.assertEqual(reversed[1].to_user_id, 3)
+
+        reversed = HBaseFollowing.filter(start=(1, reversed[1].created_at), limit=2, reverse=True)
+        self.assertEqual(len(reversed), 2)
+        self.assertEqual(reversed[0].to_user_id, 3)
+        self.assertEqual(reversed[1].to_user_id, 2)
